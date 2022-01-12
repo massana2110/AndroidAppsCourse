8c6db534350985281d040b747345ae3e98d048cf
massana2110 <00072416@uca.edu.sv>
2022-01-12 08:43:53 -0600

    My first app solution

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        rollButton.setOnClickListener { rollDice(resultText) }
+        countUpButton.setOnClickListener { addOneToDice(resultText) }
+    }
+
+    private fun rollDice(resultText: TextView) {
+        val randomInt = (1..6).random()
+        resultText.text = randomInt.toString()
+    }
+
+    // Coding Challenge:
+    // https://developer.android.com/codelabs/kotlin-android-training-app-anatomy#6
+    private fun addOneToDice(resultText: TextView) {
+
+        when (val actualValue = resultText.text.toString()) {
+            getString(R.string.message) -> resultText.text = "1"
+            "6" -> return
+            else -> {
+                resultText.text = (actualValue.toInt() + 1).toString()
+            }
+        }
+    }
+}
